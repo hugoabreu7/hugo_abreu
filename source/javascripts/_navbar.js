d7be9c26c0ec1dd967045e609cf92c0f7eafdceb
Hugo Abreu <hugoabreu90@gmail.com>
2018-12-12 17:54:40 +0900

    Finished layout fixes

--- source/javascripts/_navbar.js
+++ source/javascripts/_navbar.js
@@ -2,7 +2,7 @@ function navBar() {
   const navbar = document.querySelector('.navbar');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight * 0.9) {
+      if (window.scrollY >= window.innerHeight * 0.88) {
         navbar.classList.add('navbar-white');
       } else {
         navbar.classList.remove('navbar-white');
@@ -15,7 +15,7 @@ function navBarButton() {
   const button = document.querySelector('.nav-bar-button');
   if (button) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight * 0.9) {
+      if (window.scrollY >= window.innerHeight * 0.88) {
         button.classList.remove('nav-bar-button-secondary-hover');
         button.classList.add('nav-bar-button-colored');
       } else {
